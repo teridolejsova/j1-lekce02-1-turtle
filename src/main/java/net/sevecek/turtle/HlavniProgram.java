30d3eaa1c14216c92b04c12de80b9afc6b3b59f2
Tereza Dolejšová <terdoe>
2022-04-13 20:08:09 +0200

    zofka kresli 2 domečky

--- src/main/java/net/sevecek/turtle/HlavniProgram.java
+++ src/main/java/net/sevecek/turtle/HlavniProgram.java
@@ -6,10 +6,48 @@ public class HlavniProgram {
 
     public void main(String[] args) {
         Turtle zofka;
-
         zofka = new Turtle();
-        zofka.move(50);
 
+        for (int i = 0; i <2 ; i++) {
+
+
+
+        zofka.turnLeft(45);
+        zofka.move(100);
+        zofka.turnLeft(90);
+        zofka.move(100);
+        zofka.turnLeft(45);
+        zofka.move(150);
+        zofka.turnLeft(90);
+        zofka.move(150);
+        zofka.turnLeft(90);
+        zofka.move(150);
+        zofka.turnLeft(90);
+        zofka.move(150);
+        zofka.penUp();
+        zofka.move(200);
+        zofka.penDown();
+        zofka.turnRight(90);
+
+        }
+
+        /*
+        zofka.penDown();
+        zofka.turnRight(90);
+        zofka.turnLeft(45);
+        zofka.move(100);
+        zofka.turnLeft(90);
+        zofka.move(100);
+        zofka.turnLeft(45);
+        zofka.move(150);
+        zofka.turnLeft(90);
+        zofka.move(150);
+        zofka.turnLeft(90);
+        zofka.move(150);
+        zofka.turnLeft(90);
+        zofka.move(150);
+        zofka.penUp();
+    */
     }
 
 }
